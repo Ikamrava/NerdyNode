ad68e33882c7a345f7cc48b477b84eca3b2ef2e0
ikamrava <kamrava.iman@outlook.com>
2024-06-07 22:15:30 +0100

    featured

--- src/component/categoryList/CategoryList.jsx
+++ src/component/categoryList/CategoryList.jsx
@@ -1,9 +1,45 @@
+import Link from 'next/link'
 import styles from './categoryList.module.css'
+import Image from 'next/image'
 
 function CategoryList() {
   return (
     <div className={styles.container}>
-      CategoryList
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+          <Link href="/blog?cat=style" className={`${styles.category} ${styles.smartphones_accessories}`}>
+            <Image src="/smartphone.jpg" alt='' width={124} height={124} className={styles.image}></Image>
+            Smartphones & Accessories
+          </Link>
+
+          <Link href="/blog?cat=style" className={`${styles.category} ${styles.computers_laptops}`}>
+            <Image src="/laptop.jpg" alt='' width={124} height={124} className={styles.image}></Image>
+            Computers & Laptops
+          </Link>
+
+          <Link href="/blog?cat=style" className={`${styles.category} ${styles.wearable}`}>
+            <Image src="/wearables.jpg" alt='' width={124} height={124} className={styles.image}></Image>
+            Wearable Technology
+          </Link>
+
+          <Link href="/blog?cat=style" className={`${styles.category} ${styles.home_entertainment}`}>
+            <Image src="/home.png" alt='' width={124} height={124} className={styles.image}></Image>
+            Home Entertainment
+          </Link>
+
+          <Link href="/blog?cat=style" className={`${styles.category} ${styles.smart_home}`}>
+            <Image src="/smarthomes.jpg" alt='' width={124} height={124} className={styles.image}></Image>
+            Smart Home
+          </Link>
+
+          <Link href="/blog?cat=style" className={`${styles.category} ${styles.Gaming}`}>
+            <Image src="/gaming.jpg" alt='' width={124} height={124} className={styles.image}></Image>
+            Gaming
+          </Link>
+
+
+          
+      </div>
       
     </div>
   )
--- src/component/featured/Featured.jsx
+++ src/component/featured/Featured.jsx
@@ -1,8 +1,24 @@
+import Image from 'next/image'
 import styles from './featured.module.css'
 
 function Featured() {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, NERDYNODE here!</b> 
+        <b className={styles.discover}>Dicover my stories and creative ideas</b>
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+        <Image className={styles.image} src="/p1.jpeg" alt='' fill></Image>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h1>
+          <p className={styles.postDesc}>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <button className={styles.button}>Raed More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
