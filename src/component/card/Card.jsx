79941ec5e6296d35cd65de4d606d906d5af2597e
ikamrava <kamrava.iman@outlook.com>
2024-06-13 19:36:23 +0100

    Card in menu

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -6,16 +6,16 @@ function Card() {
   return (
     <div className={styles.container}>
           <div className={styles.imageContainer}>
-           <Image src="/p1.jpeg" alt="" fill />
+           <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
           </div>
           <div className={styles.textContainer}>
             <div className={styles.details}>
-                <span className={styles.date}>11.0.2023</span>
-                <span className={styles.category}>JAVASCRIPT</span>
+                <span className={styles.date}>11.0.2023 - </span>
+                <span className={styles.category}>JAVA SCRIPT</span>
             </div>
-            <h1 className={styles.title}>className</h1>
-             <p className={styles.desc}>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
-             <Link href = "/">Raed More</Link>
+            <h1 className={styles.title}>Neque porro quisquam</h1>
+             <p className={styles.desc}>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci"</p>
+             <Link href = "/" className={styles.link}>Raed More</Link>
           </div>
           
         
--- src/component/menu/Menu.jsx
+++ src/component/menu/Menu.jsx
@@ -1,8 +1,27 @@
+import Link from 'next/link'
 import styles from './menu.module.css'
+import Image from 'next/image'
 
 function Menu() {
   return (
-    <div className={styles.container}>Menu</div>
+    <div className={styles.container}>
+      <h2 className={styles.new}>{"What's hot"}</h2>
+      <h1>Most Popular</h1>
+      <div className={styles.items}>
+        <div className={styles.item}>
+         <Link href="/" className={styles.item}>
+         <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="food"  fill />
+         </div>
+         <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.react}`}>React</span>
+          <h3>jhsgijqsg klqwhqowh klhqlwkdh jhhd1ohd 1idh1odh1iodh iodh2iodh</h3>
+         </div>
+         </Link>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
