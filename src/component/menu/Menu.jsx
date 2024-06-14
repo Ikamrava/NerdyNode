2a7696f698f039f7a35a71255ccf6ba9956f0373
ikamrava <kamrava.iman@outlook.com>
2024-06-14 22:28:54 +0100

    category in menu

--- src/component/menu/Menu.jsx
+++ src/component/menu/Menu.jsx
@@ -5,22 +5,26 @@ import Image from 'next/image'
 function Menu() {
   return (
     <div className={styles.container}>
-      <h2 className={styles.new}>{"What's hot"}</h2>
-      <h1>Most Popular</h1>
-      <div className={styles.items}>
-        <div className={styles.item}>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      
+        <div className={styles.items}>
          <Link href="/" className={styles.item}>
-         <div className={styles.imageContainer}>
-          <Image src="/p1.jpeg" alt="food"  fill />
-         </div>
-         <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.react}`}>React</span>
-          <h3>jhsgijqsg klqwhqowh klhqlwkdh jhhd1ohd 1idh1odh1iodh iodh2iodh</h3>
-         </div>
+            <div className={styles.imageContainer}>
+              <Image src="/p1.jpeg" alt="food" fill className={styles.image} />
+            </div>
+            <div className={styles.textContainer}>
+                <span className={`${styles.category} ${styles.react}`}>React</span>
+                  <h3 className={styles.postTitle}>jhsgijqsg klqwhqowh klhqlwkdh</h3>
+                <div lassName={styles.details}>
+                    <span className={styles.userName}>John</span>
+                    <span className={styles.date}> - 10.03.2023</span>
+                </div>
+            </div>
          </Link>
         </div>
 
-      </div>
+      
     </div>
   )
 }
